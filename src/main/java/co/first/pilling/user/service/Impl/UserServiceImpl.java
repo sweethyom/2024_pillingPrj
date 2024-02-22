87e7f1ea3e96ef923408f4da9e36f0f0004c0213
sonjieun <sj5596e@gmail.com>
2024-02-22 16:37:50 +0900

    VO 추가 중

---
+++ src/main/java/co/first/pilling/user/service/Impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package co.first.pilling.user.service.Impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import co.first.pilling.user.service.UserService;
+import co.first.pilling.user.service.UserVO;
+import co.first.pilling.user.service.map.UserMapper;
+
+@Service
+@Primary
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserMapper map;
+	
+	@Override
+	public List<UserVO> userSelectList() {
+		// TODO Auto-generated method stub
+		return map.userSelectList();
+	}
+
+	@Override
+	public UserVO userSelect(UserVO vo) {
+		// TODO Auto-generated method stub
+		return map.userSelect(vo);
+	}
+
+	@Override
+	public int userInsert(UserVO vo) {
+		// TODO Auto-generated method stub
+		return map.userInsert(vo);
+	}
+
+	@Override
+	public int userDelete(UserVO vo) {
+		// TODO Auto-generated method stub
+		return map.userDelete(vo);
+	}
+
+	@Override
+	public int userUpdate(UserVO vo) {
+		// TODO Auto-generated method stub
+		return map.userUpdate(vo);
+	}
+
+}
